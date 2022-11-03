4ed5f58750dc4b52b1c409951b82f69606092341
lani-irving <laniirving@googlemail.com>
2022-11-03 17:09:36 +0000

    4 tests in place

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 //The SoC squat leaderboard
-/*We have been keeping track of how many squats our coaches perform each morning, this information is stored in a varible called "coaches". Write a function that return the three coaches with the highest score in order from best to worst!
-*/
-import { coaches } from "./main.test";
+
+/*We have been keeping track of how many squats some of our coaches perform each morning!
+Their names and scores are stored in an array called "coaches".
+Write a function that returns the three coaches with the highest score, ranked from best to worst!
+*/  
 
 
 export function squatLeaders(coaches){
-   
+
 }
-  
-
-
--- main.test.js
+++ main.test.js
@@ -45,7 +45,13 @@ const coaches = [
     }
 ];
 
+//tests return is an array
+test("Returns array", () => {
+    expect.any(Array);
+})
 
+
+//tests using the full coaches array
 test("Return top three from full array", () => {
     const actual = squatLeaders(coaches);
 
@@ -64,6 +70,82 @@ test("Return top three from full array", () => {
         }
     ]
     expect(actual).toStrictEqual(expected);
+
 })
 
+//tets using an array of 3 people
+test("Rank Gabe, Lani and Fabbi", () => {
+    const actual = squatLeaders([
+        { 
+            name: "Lani",
+            score: 12
+        },
+        {
+            name: "Fabbi",
+            score: 30
+        },
+        {
+            name: "Gabe",
+            score: 65
+        }
+    ]);
+
+    const expected = [
+        {
+            name: "Gabe",
+            score: 65
+        },
+        {
+            name: "Fabbi",
+            score: 30
+        },
+        { 
+            name: "Lani",
+            score: 12
+        }
+    ]
+    expect(actual).toStrictEqual(expected);
+
+})
+
+//tests using an array of 4 people
+test("Rank top bootcampers", () => {
+    const actual = squatLeaders([
+        {
+            name: "Bella and baby",
+            score: 120
+        },
+        {
+            name: "Taylor and chairs",
+            score: 10
+        },
+        {
+            name: "Ryan",
+            score: 5000
+        },
+        {
+            name: "Ryan",
+            score: 5001
+        }
+    ]);
+    const expected = [
+        {
+            name: "Ryan",
+            score: 5001
+        },
+        {
+            name: "Ryan",
+            score: 5000
+        },
+        {
+            name: "Bella and baby",
+            score: 120
+        }
+    ]
+    expect(actual).toStrictEqual(expected);
+} )
+
+
+
 export {coaches};
+
