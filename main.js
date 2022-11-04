06fc788f31b6355c8c4972fd52337e3c0690725f
Sam MC <sam+github@sdmjc.net>
2022-11-04 23:06:58 +0000

    fin

--- main.js
+++ main.js
@@ -3,9 +3,8 @@
 /*We have been keeping track of how many squats some of our coaches perform each morning!
 Their names and scores are stored in an array called "coaches".
 Write a function that returns the three coaches with the highest score, ranked from best to worst!
-*/  
+*/
 
-
-export function squatLeaders(coaches){
-
+export function squatLeaders(coaches) {
+  return coaches.sort((a, b) => b.score - a.score).slice(0, 3);
 }
